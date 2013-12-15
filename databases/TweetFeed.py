16bc937a236abe5365ea70f91ab85d7267dbb281
root <sujitmhj@gmail.com>
2013-12-15 16:52:04 +0545

    push

--- databases/TweetFeed.py
+++ databases/TweetFeed.py
@@ -10,15 +10,12 @@ class TweetFeed():
 
     # def select_db(self, db_name):
     #   self.db = self.client[db_name]
-   	def insert_tweet(self, value):
-   		self.table_name.insert_one(self.table_name, valu)
-   		pass
 
     def get_tweet_by_id(self,tweet_id):
         return self.db_object.get_one(self.table_name,{'tweet_id':tweet_id, 'deleted':0})
 
     def get_tweet_by_parent_id(self, parent_tweet_id):
-    	return self.db_object.get_all(self.table_name,{'parent_tweet_id':parent_tweet_id, 'deleted':0})
+    	return self.db_object.get_all(self.table_name,{'parent_tweet_id':parent_tweet_id, 'deleted':0}, 'time_stamp')
 
     def delete_tweet(self, tweet_id):
     	self.db_object.update(self.table_name,{'_id':ObjectId(tweet_id)}, {'deleted':1})
@@ -28,12 +25,25 @@ class TweetFeed():
 
 
     def insert_tweet(self, value):
+    	value['deleted'] = 0
     	value['time_stamp'] = int(time.time())
         self.db_object.insert_one(self.table_name,value)
 
 
 
-tweet = TweetFeed()
-# print tweet.delete_tweet('52ad41d3df1d2d0f0e7e7da5')
-# tweet.insert_tweet({'parent_tweet_id':0, 'tweet_message':"nepal", 'time_stamp':datetime.now(), 'deleted':0})
-print tweet.get_tweet_by_parent_id(0)
+# tweet = TweetFeed()
+# # print tweet.delete_tweet('52ad41d3df1d2d0f0e7e7da5')
+# # tweet.insert_tweet({'parent_tweet_id':0, 'tweet_message':"nepal", 'time_stamp':datetime.now(), 'deleted':0})
+# print tweet.get_tweet_by_parent_id(0)
+
+consumer_key = 'seqGJEiDVNPxde7jmrk6dQ'
+consumer_secret = 'sI2BsZHPk86SYB7nRtKy0nQpZX3NP5j5dLfcNiP14'
+c_token= '140447822-8t19LHRlaAq7NwNamkDwsqLnbvuTO4sTa2eMG3jW'
+sec = 'UGVkrG9XmpoqO93ZNdEu9mmSHHwmXTd8nVNmwwn7qFdOb'
+
+from twython import Twython
+twitter = Twython(consumer_key, consumer_secret,c_token,sec)
+
+twitter.verify_credentials()
+
+print twitter.search(q='@News24',count=2)['statuses']
