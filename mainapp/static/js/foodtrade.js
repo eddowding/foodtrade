d6426ff439157389191c498f5dd31ab7a5ded0de
SUJIT <sujitmhj@gmail.com>
2013-12-18 15:28:09 +0545

    push

---
+++ mainapp/static/js/foodtrade.js
@@ -0,0 +1,23 @@
+
+
+// Location autosuggestions
+
+var placeCord;
+function initialize() {
+
+  // Create the search box and link it to the UI element.
+  var input = /** @type {HTMLInputElement} */(
+      document.getElementById('pac-input'));
+
+  var searchBox = new google.maps.places.SearchBox(
+    /** @type {HTMLInputElement} */(input));
+
+  // [START region_getplaces]
+  // Listen for the event fired when the user selects an item from the
+  // pick list. Retrieve the matching places for that item.
+  google.maps.event.addListener(searchBox, 'places_changed', function() {
+  var places = searchBox.getPlaces();
+    placeCord = places[0].geometry.location;
+  });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
