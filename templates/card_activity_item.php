a02ba797d70ff4698ef3d8b22fdc5946a68722bb
Ed dowding <ed@foodtrade.com>
2013-12-19 14:05:54 +0000

    adding tag admin page.

--- templates/card_activity_item.php
+++ templates/card_activity_item.php
@@ -1,6 +1,6 @@
 
     <li class="left clearfix"><span class="chat-img pull-left">
-    <img src="https://pbs.twimg.com/profile_images/378800000141996074/6a363e3c4f2a84a956c3cb27c50b2ca0_normal.png" alt="User Avatar" class="img-thumbnail" />
+    <img src="https://pbs.twimg.com/profile_images/2068167135/1_normal.jpg" alt="User Avatar" class="img-thumbnail" />
     </span>
         <div class="chat-body clearfix">
  
@@ -23,8 +23,7 @@
               <li><a href="#">Follow on Twitter</a></li>
               <li><a href="#">View full profile</a></li> 
             </ul>
-          </div>  
-
+          </div>   
 
             <div class="header">
                 <strong class="pull-left" style="margin-right: 10px;">Smales Farm</strong> 
@@ -57,11 +56,224 @@
                 <span class="pull-right">
                    <a href="activity_single.php" class="text-muted"><i class="fa fa-clock-o"></i> 21m</a> 
                 </span>
+ 
+                <div class="reply well well-sm" style="margin:0;"> 
+                  <input class="form-control input-sm" type="text" placeholder="Reply to @smalesfarm  ">
+                </div>
+
               </div> 
 
         </div>
 
-        <div class="reply well well-sm" style="margin:0;"> 
-          <input class="form-control input-sm" type="text" placeholder="Reply to @smalesfarm @KagroX   @66Betty  ">
-        </div>
     </li>
+
+
+
+    <li class="left clearfix"><span class="chat-img pull-left">
+    <img src="https://pbs.twimg.com/profile_images/2068167135/1_normal.jpg" alt="User Avatar" class="img-thumbnail" />
+    </span>
+        <div class="chat-body clearfix">
+ 
+
+        <div class="btn-group pull-right" style="margin-left: 5px;">
+            <button type="button" class="btn btn-default dropdown-toggle btn-xs pull-left" data-toggle="dropdown">
+              <i class="fa fa-link fa-large text-muted"></i>
+                <span class="caret text-muted"></span>
+            </button> 
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="#">I buy from</a></li>
+              <li><a href="#">I sell to</a></li> 
+              <li class="divider"></li>
+              <li><a href="#">I'm a member</a></li>
+              <li><a href="#">I'm a customer</a></li>
+              <li class="divider"></li>
+              <li><a href="#">Delete</a></li>
+              <li><a href="#">Flag as spam</a></li>
+              <li><a href="#">dsf</a></li> 
+              <li><a href="#">Follow on Twitter</a></li>
+              <li><a href="#">View full profile</a></li> 
+            </ul>
+          </div>   
+
+            <div class="header">
+                <strong class="pull-left" style="margin-right: 10px;">Smales Farm</strong> 
+
+                <div class="tags tags-biztype">  
+                  <a href="/type/livestock-farm">Bakery</a>
+                  <a href="/type/buther">Wholesale</a>  
+                </div>
+            </div>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                dolor, quis ullamcorper ligula sodales.
+            </p>
+
+              <div class="meta small text-muted">
+               <i class="fa fa-map-marker"></i>
+                78 Example Street, Test Town   
+
+                 <!-- if logged in or you've allows html5 browser location -->
+
+                 <a href="http://maps.google.com/maps?saddr=current+location&daddr=Sydney+Opera+House,+Sydney+Opera+House,+Bennelong+Point,+Sydney+NSW+2000,+Australia" 
+                    target="_blank" data-placement="top" data-toggle="tooltip" 
+                    class="text-muted" 
+                    title="Get directions"> 
+                 <i class="fa fa-location-arrow"></i> 87.2 miles  
+                 </a>
+                <!--  // if logged in or you've allows html5 browser location -->
+
+                <span class="pull-right">
+                   <a href="activity_single.php" class="text-muted"><i class="fa fa-clock-o"></i> 21m</a> 
+                </span>
+
+                <div class="reply well well-sm" style="margin:0;"> 
+                  <input class="form-control input-sm" type="text" placeholder="Reply to @smalesfarm   ">
+                </div>
+              </div> 
+
+        </div>
+
+    </li>
+    
+
+<!-- nested replies -->
+    <li>
+      <ul>
+
+        <li class="left clearfix activity_reply"><span class="chat-img pull-left">
+        <img src="https://pbs.twimg.com/profile_images/1860239393/ipad_icon_512x512px_normal.png" alt="User Avatar" class="img-thumbnail" />
+        </span>
+            <div class="chat-body clearfix">
+     
+
+            <div class="btn-group pull-right" style="margin-left: 5px;">
+                <button type="button" class="btn btn-default dropdown-toggle btn-xs pull-left" data-toggle="dropdown">
+                  <i class="fa fa-link fa-large text-muted"></i>
+                    <span class="caret text-muted"></span>
+                </button> 
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">I buy from</a></li>
+                  <li><a href="#">I sell to</a></li> 
+                  <li class="divider"></li>
+                  <li><a href="#">I'm a member</a></li>
+                  <li><a href="#">I'm a customer</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Delete</a></li>
+                  <li><a href="#">Flag as spam</a></li>
+                  <li><a href="#">dsf</a></li> 
+                  <li><a href="#">Follow on Twitter</a></li>
+                  <li><a href="#">View full profile</a></li> 
+                </ul>
+              </div>   
+
+                <div class="header">
+                    <strong class="pull-left" style="margin-right: 10px;">Morrison's Shop</strong> 
+
+                    <div class="tags tags-biztype">  
+                      <a href="/type/livestock-farm">Bakery</a>
+                      <a href="/type/buther">Wholesale</a>  
+                    </div>
+                </div>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                    dolor, quis ullamcorper ligula sodales.
+                </p>
+
+                  <div class="meta small text-muted">
+                   <i class="fa fa-map-marker"></i>
+                    78 Example Street, Test Town   
+
+                     <!-- if logged in or you've allows html5 browser location -->
+
+                     <a href="http://maps.google.com/maps?saddr=current+location&daddr=Sydney+Opera+House,+Sydney+Opera+House,+Bennelong+Point,+Sydney+NSW+2000,+Australia" 
+                        target="_blank" data-placement="top" data-toggle="tooltip" 
+                        class="text-muted" 
+                        title="Get directions"> 
+                     <i class="fa fa-location-arrow"></i> 87.2 miles  
+                     </a>
+                    <!--  // if logged in or you've allows html5 browser location -->
+
+                    <span class="pull-right">
+                       <a href="activity_single.php" class="text-muted"><i class="fa fa-clock-o"></i> 21m</a> 
+                    </span>
+
+                    <div class="reply well well-sm" style="margin:0;"> 
+                      <input class="form-control input-sm" type="text" placeholder="Reply to @smalesfarm @morrisons  ">
+                    </div>
+ 
+                  </div> 
+ 
+            </div>
+
+        </li>
+        <li class="left clearfix activity_reply"><span class="chat-img pull-left">
+        <img src="https://pbs.twimg.com/profile_images/3077153306/e770dc0df2b6611a8ebdac412fc3e1d5_normal.jpeg" alt="User Avatar" class="img-thumbnail" />
+        </span>
+            <div class="chat-body clearfix">
+     
+
+            <div class="btn-group pull-right" style="margin-left: 5px;">
+                <button type="button" class="btn btn-default dropdown-toggle btn-xs pull-left" data-toggle="dropdown">
+                  <i class="fa fa-link fa-large text-muted"></i>
+                    <span class="caret text-muted"></span>
+                </button> 
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="#">I buy from</a></li>
+                  <li><a href="#">I sell to</a></li> 
+                  <li class="divider"></li>
+                  <li><a href="#">I'm a member</a></li>
+                  <li><a href="#">I'm a customer</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Delete</a></li>
+                  <li><a href="#">Flag as spam</a></li>
+                  <li><a href="#">dsf</a></li> 
+                  <li><a href="#">Follow on Twitter</a></li>
+                  <li><a href="#">View full profile</a></li> 
+                </ul>
+              </div>   
+
+                <div class="header">
+                    <strong class="pull-left" style="margin-right: 10px;">Euro Farm</strong> 
+
+                    <div class="tags tags-biztype">  
+                      <a href="/type/livestock-farm">Bakery</a>
+                      <a href="/type/buther">Wholesale</a>  
+                    </div>
+                </div>
+
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                    dolor, quis ullamcorper ligula sodales.
+                </p>
+
+                  <div class="meta small text-muted">
+                   <i class="fa fa-map-marker"></i>
+                    78 Example Street, Test Town   
+
+                     <!-- if logged in or you've allows html5 browser location -->
+
+                     <a href="http://maps.google.com/maps?saddr=current+location&daddr=Sydney+Opera+House,+Sydney+Opera+House,+Bennelong+Point,+Sydney+NSW+2000,+Australia" 
+                        target="_blank" data-placement="top" data-toggle="tooltip" 
+                        class="text-muted" 
+                        title="Get directions"> 
+                     <i class="fa fa-location-arrow"></i> 87.2 miles  
+                     </a>
+                    <!--  // if logged in or you've allows html5 browser location -->
+
+                    <span class="pull-right">
+                       <a href="activity_single.php" class="text-muted"><i class="fa fa-clock-o"></i> 21m</a> 
+                    </span>
+
+                    <div class="reply well well-sm" style="margin:0;"> 
+                      <input class="form-control input-sm" type="text" placeholder="Reply to @smalesfarm @morrisons @eurofarm">
+                    </div>
+                  </div> 
+
+            </div>
+
+        </li>
+</ul>
+</li>
+
