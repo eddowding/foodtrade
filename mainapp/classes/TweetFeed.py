4aca864f93f90b67f97ef2566072858842d8078f
Roshan Bhandari <brishi98@gmail.com>
2014-01-06 17:01:24 +0545

    Trending Hashtags Lising near successful

--- mainapp/classes/TweetFeed.py
+++ mainapp/classes/TweetFeed.py
@@ -156,5 +156,28 @@ class Customer():
         self.db_object.insert_one(self.table_name,value)
 
     def delete_customer(self, useruid, customer_id):
-        self.db_object.update(self.table_name,{'useruid': useruid, 'customer_id': customer_id}, {'deleted':1})
+        self.db_object.update(self.table_name,{'useruid': useruid, 'customeruid': customer_id}, {'deleted':1})
 
+class Organisation():
+    """docstring for Connection"""
+    def __init__(self):
+        self.db_object = MongoConnection("localhost",27017,'foodtrade')
+        self.table_name = 'organisation'
+        self.db_object.create_table(self.table_name,'orguid')
+
+    def get_members_by_orgid(self,orguid):
+        return self.db_object.get_all(self.table_name,{'orguid': orguid, 'deleted': 0})
+
+    def create_member (self, value):
+        value['deleted'] =0
+        self.db_object.insert_one(self.table_name,value)
+
+    def delete_member(self, orguid, member_id):
+        self.db_object.update(self.table_name,{'orguid': orguid, 'memberuid': member_id}, {'deleted':1})
+
+    def get_organisations_by_mem_id(self, member_id):
+        return self.db_object.get_all(self.table_name,{'memberuid': member_id, 'deleted': 0})
+
+# trade_conn = TradeConnection()
+# trade_conn.create_connection({'b_useruid': 23, 'c_useruid': 20})
+# print trade_conn.get_connection_by_customer(23)
