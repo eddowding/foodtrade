cd7b730731bff75bf1551dc0b49348de7ab691b6
Ed dowding <ed@foodtrade.com>
2014-01-08 22:51:20 +0000

    added Invite mockups for individual, org, and business

--- templates/invite.php
+++ templates/invite.php
@@ -12,96 +12,132 @@
 <div class="container">
     <div class="row">
 
+    
+
+ 
+
  <div class="panel-body">
-                    <ul class="list-group">
-                        <li class="list-group-item clearfix">
-                            <div class="pull-right action-buttons">
-                             <button class="btn btn-default">Buys from</button>
-                             <button class="btn btn-default">Sells to</button>
-                            </div>
-                            <div class="checkbox">
-                                <input type="checkbox" id="checkbox" />
-                                <label for="checkbox">
-                                     <img src="https://pbs.twimg.com/profile_images/67617802/thoughtful_logo_bigger.jpg" alt="THEIRNAME"
-          class=" img-responsive pull-left" style="width:20px; margin-right: 5px;" /> 
-                                    List group item heading
-                                </label>
-                            </div>
-                        </li>
-                        <li class="list-group-item clearfix">
-                            <div class="pull-right action-buttons">
-                             <button class="btn btn-default">Buys from</button>
-                             <button class="btn btn-default">Sells to</button>
-                            </div>
-                            <div class="checkbox">
-                                <input type="checkbox" id="checkbox" />
-                                <label for="checkbox">
-                                    List group item heading
-                                </label>
-                            </div>
-                        </li>
-                        <li class="list-group-item clearfix">
-                            <div class="pull-right action-buttons">
-                             <button class="btn btn-default">Buys from</button>
-                             <button class="btn btn-default">Sells to</button>
-                            </div>
-                            <div class="checkbox">
-                                <input type="checkbox" id="checkbox" />
-                                <label for="checkbox">
-                                    List group item heading
-                                </label>
-                            </div>
-                        </li>
-                        
-                        </ul>
-                        </div>
+<ul class="list-group">
 
 
-        <div class="col-md-12 well well-sm">
-            <form action="#" method="get">
-                <div class="input-group">
-                    <!-- USE TWITTER TYPEAHEAD JSON WITH API TO SEARCH -->
-                    <input class="form-control" id="system-search" name="q" placeholder="Search for" required>
-                    <span class="input-group-btn">
-                        <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
-                    </span>
-                </div>
-            </form>
+
+    <li class="list-group-item clearfix" style="background: #eee;">
+    <button class="btn btn-primary pull-right">Send 2 invites</button>
+<form action="#" method="get" class="col-md-6">
+          <div class="input-group">
+            <!-- USE TWITTER TYPEAHEAD JSON WITH API TO SEARCH -->
+            <input class="form-control" id="system-search" name="q" placeholder="Search for" required>
+            <span class="input-group-btn">
+                <button type="submit" class="btn btn-default"><i class="glyphicon glyphicon-search"></i></button>
+            </span>
+          </div>
+        </form>
+    </li>
+    
+
+    <li class="list-group-item">
+      
+      <!-- only show this if checkbox selected --> 
+      <div class="pull-right">
+        
+        <img src="http://pbs.twimg.com/profile_images/378800000141996074/6a363e3c4f2a84a956c3cb27c50b2ca0_normal.png" alt="THEIRNAME" class=" img-responsive   pull-left" style="width:33px; margin: 0 5px;" />  
+             
+        <div class="btn-group pull-left" data-toggle="buttons">
+          <label class="btn btn-default">
+            <input type="checkbox"> Buys from
+          </label>
+          <label class="btn btn-default">
+            <input type="checkbox"> Sells to
+          </label> 
         </div>
-        <div class="col-md-12">
-         <table class="table table-list-search">
-            <thead>
-                <tr>
-                    <th>cb</th>
-                    <th>av / name</th>
-                    <th>action</th> 
-                </tr>
-            </thead>
-            <tbody style="height: 300px; overflow: scroll-y;">
-                <tr>
-                    <td colspan="3">
-                     <div class="checkbox">
-                        <input type="checkbox" id="checkbox" />
-                        <label for="checkbox">
-                            List group item heading
-                        </label>
-                     </div>
-                    <div class="pull-right">
-                     <button class="btn btn-default">Buys from</button>
-                     <button class="btn btn-default">Sells to</button>
-                    </div>
 
-                    </td> 
-                </tr>
-               
-            </tbody>
-            <tfoot>
-                <tr>
-                    <td colspan="3" class="text-right"><button class="btn btn-primary">Invite selected</button>
-                </tr>
-            </tfoot>
-        </table>   
+        <img src="https://pbs.twimg.com/profile_images/67617802/thoughtful_logo_bigger.jpg" alt="THEIRNAME" class=" img-responsive  pull-left " style="width:33px; margin:  0 5px;" />
+ 
+      </div>
+
+      <div class="checkbox">
+        <input type="checkbox" checked="" id="checkbox" />
+        <label for="checkbox">
+          <img src="https://pbs.twimg.com/profile_images/67617802/thoughtful_logo_bigger.jpg" alt="THEIRNAME" 
+          class="img-responsive pull-left" style="width:20px; margin-right: 5px;" /> 
+          Thoughtful Bread <span class="text-muted small">@thoughtfulbread</span>
+          </label>
+      </div>
+    </li>
+
+    <li class="list-group-item">
+      
+      <!-- only show this if checkbox selected --> 
+      <div class="pull-right">
+        
+        <img src="http://pbs.twimg.com/profile_images/378800000141996074/6a363e3c4f2a84a956c3cb27c50b2ca0_normal.png" alt="THEIRNAME" class=" img-responsive   pull-left" style="width:33px; margin: 0 5px;" />  
+             
+        <div class="btn-group pull-left" data-toggle="buttons">
+          <label class="btn btn-default">
+            <input type="checkbox"> is a member of
+          </label> 
         </div>
+
+        <img src="https://pbs.twimg.com/profile_images/1690823879/Screen_shot_2011-12-13_at_12.21.20_normal.png" alt="THEIRNAME" class=" img-responsive  pull-left " style="width:33px; margin:  0 5px;" />
+ 
+      </div>
+
+      <div class="checkbox">
+        <input type="checkbox" id="checkbox" />
+        <label for="checkbox">
+          <img src="https://pbs.twimg.com/profile_images/1690823879/Screen_shot_2011-12-13_at_12.21.20_normal.png" alt="THEIRNAME"
+class=" img-responsive pull-left" style="width:20px; margin-right: 5px;" /> 
+          Slow Food UK <span class="text-muted small">@slowfooduk</span>
+        </label>
+      </div>
+    </li>
+
+    <li class="list-group-item">
+      
+      <!-- only show this if checkbox selected --> 
+      <div class="pull-right">
+           <img src="https://pbs.twimg.com/profile_images/137269542/twitter_bigger.jpg" alt="THEIRNAME" class=" img-responsive  pull-left " style="width:33px; margin:  0 5px;" />
+  
+        <div class="btn-group pull-left" data-toggle="buttons">
+          <label class="btn btn-default">
+            <input type="checkbox"> shops at
+          </label>
+          <label class="btn btn-default">
+            <input type="checkbox"> works at
+          </label> 
+        </div>
+
+        
+        <img src="http://pbs.twimg.com/profile_images/378800000141996074/6a363e3c4f2a84a956c3cb27c50b2ca0_normal.png" alt="THEIRNAME" class=" img-responsive   pull-left" style="width:33px; margin: 0 5px;" />  
+         
+      </div>
+
+      <div class="checkbox">
+        <input type="checkbox" checked="" id="checkbox" />
+        <label for="checkbox">
+          <img src="https://pbs.twimg.com/profile_images/137269542/twitter_bigger.jpg" alt="THEIRNAME"
+class=" img-responsive pull-left" style="width:20px; margin-right: 5px;" /> 
+          Ed Dowding <span class="text-muted small">@eddowding</span>
+        </label>
+      </div>
+    </li>
+
+
+      <li class="list-group-item clearfix" style="background: #eee;">
+        <button class="btn btn-primary pull-right">Send 2 invites</button>
+      </li>
+                 
+
+  </ul>
+</div>
+
+
+      
+
+
+
+
+
     </div>
 </div>
 
