36ac94e0e378846a004f5836911d641f8866e3ad
Santosh Ghimire <santosh.ghimire33@gmail.com>
2014-01-13 00:56:11 +0545

    add food bug fixed

--- mainapp/classes/TweetFeed.py
+++ mainapp/classes/TweetFeed.py
@@ -491,7 +491,7 @@ class Food():
     def create_food (self, value):
         value['deleted'] =0
         # self.db_object.insert_one(self.table_name,value)
-        self.db_object.update_upsert(self.table_name, {'food_name': value['food_name']}, {'deleted': 0, 'useruid': value['useruid']})
+        self.db_object.update_upsert(self.table_name, {'food_name': value['food_name'], 'useruid': value['useruid']}, {'deleted': 0})
 
     def delete_food(self, useruid, food_name):
         self.db_object.update(self.table_name,{'useruid': useruid, 'food_name': food_name}, {'deleted':1})
