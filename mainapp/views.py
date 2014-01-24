2ea1e4ee1ef722235a82678d622fd4c1bed6dc1f
Santosh Ghimire <santosh.ghimire33@gmail.com>
2014-01-24 17:47:29 +0545

    nth

--- mainapp/views.py
+++ mainapp/views.py
@@ -101,10 +101,10 @@ def tweets(request):
         except:
             text = "@" + tweet['user']['screen_name'] + " Thanks! Please confirm your post by clicking this http://fresh.foodtrade.com/?" + tweet['id_str'] + " You'll only have to do this once."
             print text
-            try:
-                bot_twitter.update_status(status = text, in_reply_to_status_id = tweet['id'])
-            except:
-                pass
+            # try:
+            #     bot_twitter.update_status(status = text, in_reply_to_status_id = tweet['id'])
+            # except:
+            #     pass
 
     if len(tweet_list)!=0:
         max_id.max_tweet_id = max(tweet_list)
