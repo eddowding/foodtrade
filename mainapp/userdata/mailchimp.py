40c54f75229c561409b88ee63fd117ee64d85fdc
Roshan Bhandari <brishi98@gmail.com>
2014-02-06 23:45:05 +0545

    small change in invites

--- mainapp/userdata/mailchimp.py
+++ mainapp/userdata/mailchimp.py
@@ -7,7 +7,7 @@ class MailChimp():
         self.format = 'json'
         self.api_end_point = 'https://us3.api.mailchimp.com/2.0/'
         self.api_key = 'e9294366710f56f159569b82660f9df3-us3'
-        self.eid = ''
+        self.list_id = '53c16fcfcb'
 
     def make_request(self, post_url, payload):
         r = requests.post(post_url,params=payload)
@@ -17,6 +17,6 @@ class MailChimp():
 
     def subscribe(self, doc):
         subscribe_url = self.api_end_point + 'lists/subscribe' + self.format
-
+        
         response = self.make_request(subscribe_url, payload)
 
