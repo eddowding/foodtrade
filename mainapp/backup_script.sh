5b72498aa6dc85b554bb711dbbd132820ad82add
Roshan Bhandari <brishi98@gmail.com>
2014-02-10 13:01:39 +0545

    changes

--- mainapp/backup_script.sh
+++ mainapp/backup_script.sh
@@ -3,16 +3,16 @@
 #database backup
 cd /home/backups/
 cd d1 
-rm -r dump
+sudo rm -r dump
 cd ..
-cp d2 -r d1
+sudo cp d2 -r d1
 cd d2 
-rm -r dump
+sudo rm -r dump
 cd ..
-cp d3 -r d2
+sudo cp d3 -r d2
 cd d3
 mongodump
-mysql -u root -proot foodtrade > foodtrade.sql 
+mysqldump -u root -proot foodtrade > foodtrade.sql 
 
 #run daily email sending script
 #python /srv/www/foodtrade-env/foodtrade/mainapp/notification-email.py
