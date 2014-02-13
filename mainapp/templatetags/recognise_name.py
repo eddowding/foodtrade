665a2e107a346899dba93c216f2e02ed5cb437a2
Santosh Ghimire <santosh.ghimire33@gmail.com>
2014-02-13 17:15:40 +0545

    parse #tags

--- mainapp/templatetags/recognise_name.py
+++ mainapp/templatetags/recognise_name.py
@@ -6,6 +6,10 @@ register = template.Library()
 @register.filter
 def recognise_name(value):
     result = re.findall(r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)', value, re.M|re.I)
+    tags = re.findall(r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))#([A-Za-z]+[A-Za-z0-9]+)', value, re.M|re.I)
+    if tags:
+    	for each_tag in tags:
+    		value = value.replace("#"+each_tag, '<a href="/activity/?q='+each_tag+'/">#'+each_tag+'</a>')
     if result:
         for each in result:
             try:
