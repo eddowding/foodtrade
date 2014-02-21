3afd70a11255e2c40cda17bc3e8f239e44bf9312
Roshan Bhandari <brishi98@gmail.com>
2014-02-21 23:17:15 +0545

    changes

--- mainapp/views.py
+++ mainapp/views.py
@@ -597,7 +597,7 @@ def send_newsletter(request, substype):
         tem_con = str(render_to_response('activity-email.html',{'results':results}, context_instance=RequestContext(request)))
         tem_con = tem_con.replace('Content-Type: text/html; charset=utf-8', '')
         m = Email()
-        m.send_mail("Recent FoodTrade activity near you", [{'name':'main', 'content':tem_con}], [{'email':'brishi98@gmail.com'}])
+        m.send_mail("Recent FoodTrade activity near you", [{'name':'main', 'content':tem_con}], [{'email':eachUser['email']}])
 
 
         
