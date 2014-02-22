d921214d95eff1148b48f20a2256fb5bf66da7f4
Roshan Bhandari <brishi98@gmail.com>
2014-02-22 21:28:04 +0545

    pull

--- CronJobs/daily.py
+++ CronJobs/daily.py
@@ -1,3 +1,3 @@
 import urllib2
-baseurl = "http://foodtradelite.cloudapp.net/send-newsletter/daily?code=11foodtradeESRS22"
+baseurl = "http://ftstaging.cloudapp.net/send-newsletter/daily?code=11foodtradeESRS22"
 response = urllib2.urlopen(baseurl)
