722a27c30c77ba427c5489db375be2dfe119e382
Roshan Bhandari <brishi98@gmail.com>
2014-03-20 10:38:50 +0545

    changes in kpi title

--- CronJobs/friendstousr.py
+++ CronJobs/friendstousr.py
@@ -76,18 +76,18 @@ class Friends():
 
     def register_all_friends(self):
         user_pages_count = int(self.db_object.get_count(self.table_name, {})/15)+ 1
-        # count = 0
         for i in range(0,user_pages_count, 1):
             pag_users = self.db_object.get_paginated_values(self.table_name, {}, pageNumber = int(i+1))
             for eachUser in pag_users:
-                # count = count + 1
-                # if count < 1000:
                 user_profile_obj = UserProfile(host=REMOTE_SERVER, port=27017, db_name=REMOTE_MONGO_DBNAME, 
     conn_type='remote', username=REMOTE_MONGO_USERNAME, password=REMOTE_MONGO_PASSWORD)
                 check = user_profile_obj.get_profile_by_username(eachUser['friends']['screen_name'])
                 print eachUser['friends']['screen_name']
                 if check == None:
                     register_user_to_mongo(eachUser['friends'])
+                else:
+
+    # def update_friend(screen_name):
 
 fr = Friends()        
 fr.register_all_friends()
