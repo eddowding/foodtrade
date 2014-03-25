5eab24857c79f9e775b310821017cfa910b9a928
SUJIT <sujitmhj@gmail.com>
2014-03-25 20:10:44 +0545

    hash and mention

--- mainapp/templatetags/recognise_name.py
+++ mainapp/templatetags/recognise_name.py
@@ -7,39 +7,39 @@ register = template.Library()
 
 @register.filter
 def recognise_name(value):
-    # result = re.findall(r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)', value, re.M|re.I)
-    # tags = re.findall(r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))#([A-Za-z]+[A-Za-z0-9]+)', value, re.M|re.I)
+    result = re.findall(r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))@([A-Za-z]+[A-Za-z0-9]+)', value, re.M|re.I)
+    tags = re.findall(r'(?<=^|(?<=[^a-zA-Z0-9-_\.]))#([A-Za-z]+[A-Za-z0-9]+)', value, re.M|re.I)
     # links = re.findall("((http:|https:)//[^ \<]*[^ \<\.])",value)
     # if links:
     #     for each_link in links:
     #         value = value.replace(str(each_link[0]), '<a href="'+str(each_link[0])+'" target="_blank">'+ str(each_link[0]) + '</a>')
-    # if tags:
-    # 	for each_tag in tags:
-    # 		value = value.replace("#"+each_tag, '<a href="/activity/?q=%23'+each_tag+'">#'+each_tag+'</a>')
-    # if result:
-    #     user_prof = UserProfile()
-    #     for each in result:
-    #         try:
-    #             # usr = User.objects.get(username = str(each))
-    #             usr_pr = user_prof.get_profile_by_username(each)
-    #             if usr_pr['sign_up_as'] != 'unclaimed':
-    #                 value = value.replace("@"+each, '<a href="/profile/'+each+'/">@'+each+'</a>')
-    #         except:
-    #             pass
+    if tags:
+    	for each_tag in tags:
+    		value = value.replace("#"+each_tag, '<a href="/activity/?q=%23'+each_tag+'">#'+each_tag+'</a>')
+    if result:
+        user_prof = UserProfile()
+        for each in result:
+            try:
+                # usr = User.objects.get(username = str(each))
+                usr_pr = user_prof.get_profile_by_username(each)
+                if usr_pr['sign_up_as'] != 'unclaimed':
+                    value = value.replace("@"+each, '<a href="/profile/'+each+'/">@'+each+'</a>')
+            except:
+                pass
 
 
-    # admin_foods = AdminFoods()
-    # master_foods = admin_foods.get_tags()
-    # #create master list of foods
-    # final_master = []
-    # for each in master_foods:
-    #     if each.get('childrens')!=None:
-    #         final_master.extend([str(i['node']).lower() for i in each['childrens']])
-    #     else:
-    #         final_master.extend([str(each['node']).lower()])
+    admin_foods = AdminFoods()
+    master_foods = admin_foods.get_tags()
+    #create master list of foods
+    final_master = []
+    for each in master_foods:
+        if each.get('childrens')!=None:
+            final_master.extend([str(i['node']).lower() for i in each['childrens']])
+        else:
+            final_master.extend([str(each['node']).lower()])
 
-    # val_list = value.split()
-    # for each in val_list:
-    #     if each.lower() in final_master:
-    #         value = value.replace(each, '<a href="/activity/?f='+each.title()+'">'+each+'</a>')
+    val_list = value.split()
+    for each in val_list:
+        if each.lower() in final_master:
+            value = value.replace(each, '<a href="/activity/?f='+each.title()+'">'+each+'</a>')
     return value
