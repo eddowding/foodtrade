cf945d94c877bb16ea17194029fd7e827d60ef85
Roshan Bhandari <brishi98@gmail.com>
2014-04-01 17:59:10 +0545

    changes

--- CronJobs/daily.sh
+++ CronJobs/daily.sh
@@ -2,4 +2,4 @@
 python /srv/www/live/foodtrade-env/foodtrade/CronJobs/notification_email.py
 python /srv/www/live/foodtrade-env/foodtrade/CronJobs/log_kpi.py
 python /srv/www/live/foodtrade-env/foodtrade/CronJobs/daily.py
-python /srv/www/live/foodtrade-env/foodtrade/CronJobs/usrpull.py
+#python /srv/www/live/foodtrade-env/foodtrade/CronJobs/usrpull.py
