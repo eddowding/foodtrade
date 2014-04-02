b74d4cc0bdf9561b4477cb8c9de0b3aea1e48da4
SUJIT <sujitmhj@gmail.com>
2014-04-02 17:05:20 +0545

    changes

--- mainapp/static/js/activity-map.js
+++ mainapp/static/js/activity-map.js
@@ -5,7 +5,7 @@
  var openspaceLayer = L.tileLayer.osopenspace("F481BBF739A6038DE0430B6CA40AB6D2", {debug: true}); 
 
       // map.addLayer(openspaceLayer);
-      var show_os_map = true;
+      var show_os_map = false;
 
 if(map_lat>49.89193 && map_lat<61.08466 && map_lon>-9.38053 && map_lon<2.07316&&show_os_map)
 {
@@ -66,16 +66,22 @@ $("#map").on('click dblclick keyup mousedown mousewheel', function() {
     	var current_center = map.getCenter();
     	var cent_lon = current_center.lng;
     	var cent_lat = current_center.lat;
+    	var current_zoom_level = map.getZoom();
 
     	var is_current_base = (map.options.crs == L.CRS.EPSG3857);
-
+    	var switching_zoom_level = 7;
+// Check if the view is set to UK 
 	if(cent_lat>49.89193 && cent_lat<61.08466 && cent_lon>-9.38053 && cent_lon<2.07316&&show_os_map)
     {
-
+    	// Check if it is showing non OS map
     	if(is_current_base)
     	{
-    		var over_zoom = map.getZoom()%5;
-    		if(over_zoom>0)
+    		var over_zoom = current_zoom_level-5;
+    		if(over_zoom>9)
+    		{
+    			over_zoom = 9;
+    		}
+    		if(over_zoom - switching_zoom_level>0)
     		{
     			map.options.crs=L.OSOpenSpace.getCRS();
 	    		map.removeLayer(base_layer)
@@ -87,25 +93,25 @@ $("#map").on('click dblclick keyup mousedown mousewheel', function() {
     	 else
 	    {
 
-	    	if(map.getZoom()<1)
+	    	if(current_zoom_level<switching_zoom_level+1)
 	    	{
 	    			    	
 	    		map.options.crs=L.CRS.EPSG3857;
 	    		map.removeLayer(openspaceLayer)
 	    		map.addLayer(base_layer);
-    			map.setView([cent_lat,cent_lon],5);
+    			map.setView([cent_lat,cent_lon],current_zoom_level+5);
 	    	}
 	    	
 	    }
     }
     else
     {
-    	if(map.getZoom()<1&&(!is_current_base))
+    	if(current_zoom_level<1+switching_zoom_level&&(!is_current_base))
     	{
     		map.options.crs=L.CRS.EPSG3857;
     		map.removeLayer(openspaceLayer)
     		map.addLayer(base_layer);
-			map.setView([cent_lat,cent_lon],5);
+			map.setView([cent_lat,cent_lon],current_zoom_level+5);
     	}
     }        
 });
