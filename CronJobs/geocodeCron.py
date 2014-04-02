e07bfd1c34473c5a46586cb1b5291bbeccd8ab42
Roshan Bhandari <brishi98@gmail.com>
2014-04-02 15:42:38 +0545

    changes

--- CronJobs/geocodeCron.py
+++ CronJobs/geocodeCron.py
@@ -3,11 +3,11 @@
 
 import sys
 
-# CLASS_PATH = '/srv/www/live/foodtrade-env/foodtrade/CronJobs'
-# SETTINGS_PATH = '/srv/www/live/foodtrade-env/foodtrade/foodtrade'
+CLASS_PATH = '/srv/www/live/foodtrade-env/foodtrade/CronJobs'
+SETTINGS_PATH = '/srv/www/live/foodtrade-env/foodtrade/foodtrade'
 
-CLASS_PATH = 'C:/Users/Roshan Bhandari/Desktop/foodtrade/mainapp/classes'
-SETTINGS_PATH = 'C:/Users/Roshan Bhandari/Desktop/foodtrade/foodtrade'
+# CLASS_PATH = 'C:/Users/Roshan Bhandari/Desktop/foodtrade/mainapp/classes'
+# SETTINGS_PATH = 'C:/Users/Roshan Bhandari/Desktop/foodtrade/foodtrade'
 
 sys.path.insert(0, CLASS_PATH)
 sys.path.insert(1,SETTINGS_PATH)
