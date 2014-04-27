138f0a1fa7d274a5ccbd298166080057cca96367
Roshan Bhandari <brishi98@gmail.com>
2014-04-27 16:36:44 +0545

    changes pull

--- mainapp/profilepage.py
+++ mainapp/profilepage.py
@@ -831,13 +831,13 @@ def get_video_html(url):
         return ''
     else:
         html = obj.get('html') if obj.get('html')!=None else ''
-        print html
         width_start = html.find("width=\"") 
         width_end = html[width_start+7:].find('"')
         html = html.replace(html[width_start:(width_start+7+width_end+1)], "width=\"100%\"")
+
         height_start = html.find("height=\"") 
         height_end = html[height_start+7:].find('"')
-        html = html.replace(html[width_start:(height_start+7+height_end+1)], "height=\"100%\"")
+        html = html.replace(html[height_start:(height_start+8+height_end+3)], "height=\"100%\"")
         return html
 
     
