8089cb5eb759db7878d15328b6e599b3ecd7b6fc
Roshan Bhandari <brishi98@gmail.com>
2014-05-01 19:47:59 +0545

    changes

--- mainapp/profilepage.py
+++ mainapp/profilepage.py
@@ -1098,7 +1098,75 @@ def distance(lon1, lat1, lon2, lat2):
     c = 2 * asin(sqrt(a)) 
     km = 6367 * c
     return km
-  
+
+
+def get_views_parameters(request, tweet_id):
+    from mainapp.classes.Search import Search
+    parameters={}
+    user_profile_obj = UserProfile()
+    parameters.update(csrf(request))
+    if request.user.is_authenticated():
+        user_id = request.user.id
+        user_profile = user_profile_obj.get_profile_by_id(str(user_id))
+        default_lon = float(user_profile['latlng']['coordinates'][0])
+        default_lat = float(user_profile['latlng']['coordinates'][1])
+        user_info = UserInfo(user_id)
+        parameters['userinfo'] = user_info
+    else:
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get('REMOTE_ADDR')
+        location_info = get_addr_from_ip(ip)
+        default_lon = float(location_info['longitude'])
+        default_lat = float(location_info['latitude'])
+
+    search_handle = Search(lon = default_lon, lat =default_lat)
+    try:
+        single_tweet = search_handle.get_single_tweet(str(tweet_id))
+        a = single_tweet[0]
+    except:
+        raise Http404
+    keyword = ''
+    from mainapp.activity import set_time_date
+    single_tweet = set_time_date(single_tweet[0],keyword)
+    results = search_handle.get_direct_children(str(tweet_id))
+
+    # send banner url
+    from mainapp.profilepage import get_banner_url
+    parameters['banner_url'] = get_banner_url(username=single_tweet['user']['username'], logged_useruid=request.user.id)
+    if results!= None:
+        for i in range(len(results)):
+            results[i] = set_time_date(results[i],keyword)
+            mentions = "@" + single_tweet['user']['username']+ " " + "@" + results[i]['user']['username'] 
+            results[i]['mentions'] = mentions
+
+            if results[i]["result_type"] == results[i]["user"]["username"]:
+                tweet_id = results[i]["tweetuid"]
+                replies = search_handle.get_all_children([tweet_id])
+                if replies == None:
+                    continue
+                replies = sorted(replies, key=lambda k: k['time_stamp']) 
+                for j in range(len(replies)):
+                    replies[j] = set_time_date(replies[j],keyword)
+                    replies[j]['mentions'] = mentions + " " + replies[j]['mentions']
+                results[i]['replies'] = replies
+    single_tweet['user']['profile_img'] = single_tweet['user']['profile_img'].replace("normal","bigger")
+    try:
+        single_tweet['user']['business_org_name'] = str(single_tweet['user']['business_org_name'])
+    except:
+        pass
+    parameters['results'] = results
+    parameters['json_data'] = json.dumps(results)
+    parameters['parent_tweet'] = single_tweet
+    addr = single_tweet['user']['address'].split(',')
+    parameters['tweet_country'] = addr[len(addr)-1].strip()
+    parameters['parent_json'] = json.dumps(single_tweet)
+    parameters['s_userinfo'] = UserInfo(single_tweet['useruid'])
+
+    return parameters
+
 def get_views_count(request, username):
     userprof = UserProfile()
     user= userprof.get_profile_by_username(username)
@@ -1109,6 +1177,5 @@ def get_views_count(request, username):
     parameters ={}
     parameters['user'] = user
     parameters['visit_stats'] = visit_stats
-
-
-    return render_to_response('view_stats.html', parameters, context_instance=RequestContext(request))
+    tweet_id = '443425161815932929'
+    return render_to_response('view_stats.html', get_views_parameters(request,tweet_id), context_instance=RequestContext(request))
