037bccb9065bcb1734a4330f096622bc99c3b8c4
SUJIT <sujitmhj@gmail.com>
2014-05-03 11:39:00 +0545

    final stockist updates

--- foodtrade/urls.py
+++ foodtrade/urls.py
@@ -53,6 +53,8 @@ urlpatterns = patterns('',
     url(r'^(?P<username>\w{1,40})/visitors', 'mainapp.profilepage.get_views_count'),
     ## any user    
     url(r'^(?P<username>\w{1,40})/$', 'mainapp.profilepage.profile_url_resolve'),
+    url(r'^suppliers-activity/$', 'mainapp.activity.activity_suppliers'),
+
 )
 urlpatterns += patterns('',
         (r'^media/(?P<path>.*)$', 'django.views.static.serve', {
