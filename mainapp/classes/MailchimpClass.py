bb394d64351d9b214defa5c77e7cac9cd356c977
Roshan Bhandari <brishi98@gmail.com>
2014-05-16 18:18:02 +0545

    changes

--- mainapp/classes/MailchimpClass.py
+++ mainapp/classes/MailchimpClass.py
@@ -21,35 +21,35 @@ class MailChimpClass():
         self.db_object.update_upsert(self.table_name, doc['data_sent'], doc)
 
     def subscribe(self, doc):
+        # try:
         try:
-            try:
-                first, last = doc['name'].split(' ')[0], doc['name'].split(' ')[1]
-            except:
-                first, last = doc['name'].split(' ')[0], ''
-            m = self.get_mailchimp_api()
-            if self.list_id != 'eeea3ac4c6':
-                response = m.lists.subscribe(self.list_id, email = {'email':str(doc['email'])}, 
-                    double_optin = False,
-                    merge_vars = {"FNAME":str(first),"LNAME":str(last),
-                    "groupings": [{"name": "Foodtrade Group","groups": [str(doc["sign_up_as"])]}],
-                    "mc_location":{"latitude":str(doc["latlng"]["coordinates"][1]),"longitude":str((doc["latlng"]["coordinates"][1]))}},
-                    update_existing=True, send_welcome=True, replace_interests=True)
-            else:
-                response = m.lists.subscribe(self.list_id, email = {'email':str(doc['email'])}, 
-                    double_optin = False,
-                    merge_vars = {"FNAME":str(first),"LNAME":str(last),
-                    "groupings": [{"name": "Foodtrade Group","groups": [str(doc["sign_up_as"])]}],
-                    'UserType':str(doc['sign_up_as']),
-                    'Name':str(doc['name']),
-                    'Postcode':str(doc['zip_code']),
-                    "mc_location":{"latitude":str(doc["latlng"]["coordinates"][1]),"longitude":str((doc["latlng"]["coordinates"][1]))}},
-                    update_existing=True, send_welcome=True, replace_interests=True)                
-            self.save_mailchimp_response({'data_sent':doc, 'response':response})
-            return {'status':1}
-
+            first, last = doc['name'].split(' ')[0], doc['name'].split(' ')[1]
         except:
-            print "Exception on " + doc['email']
-            return {'status':0,'message':'Already subscribed'}
+            first, last = doc['name'].split(' ')[0], ''
+        m = self.get_mailchimp_api()
+        if self.list_id != 'eeea3ac4c6':
+            response = m.lists.subscribe(self.list_id, email = {'email':str(doc['email'])}, 
+                double_optin = False,
+                merge_vars = {"FNAME":str(first),"LNAME":str(last),
+                "groupings": [{"name": "Foodtrade Group","groups": [str(doc["sign_up_as"])]}],
+                "mc_location":{"latitude":str(doc["latlng"]["coordinates"][1]),"longitude":str((doc["latlng"]["coordinates"][1]))}},
+                update_existing=True, send_welcome=True, replace_interests=True)
+        else:
+            response = m.lists.subscribe(self.list_id, email = {'email':str(doc['email'])}, 
+                double_optin = False,
+                merge_vars = {"FNAME":str(first),"LNAME":str(last),
+                "groupings": [{"name": "Foodtrade Group","groups": [str(doc["sign_up_as"])]}],
+                'UserType':str(doc['sign_up_as']),
+                'Name':str(doc['name']),
+                'Postcode':str(doc['zip_code']),
+                "mc_location":{"latitude":str(doc["latlng"]["coordinates"][1]),"longitude":str((doc["latlng"]["coordinates"][1]))}},
+                update_existing=True, send_welcome=True, replace_interests=True)                
+        self.save_mailchimp_response({'data_sent':doc, 'response':response})
+        return {'status':1}
+
+        # except:
+        #     print "Exception on " + doc['email']
+        #     return {'status':0,'message':'Already subscribed'}
 
 class MailChimpException():
     """This class is used to make api calls to the MailChimp"""
