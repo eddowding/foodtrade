0ad3d172cfaab98cbda61274ac6f0b8d517eaf9c
Roshan Bhandari <brishi98@gmail.com>
2014-06-26 13:12:20 +0545

    changes

--- CronJobs/geo.py
+++
@@ -1,6 +0,0 @@
-from pygeocoder import Geocoder
-a = Geocoder(
-	client_id = '352528416515-r1du34cl9j9kqf4cv0dc8pnjnmi6qj9l.apps.googleusercontent.com', 
-	api_key = 'AIzaSyDRNTE8EWOsbZzAQcM3hlBpaNA0zTuVups'
-)
-b = a.geocode('kahmandu')
