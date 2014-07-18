9e0a2de9f4e0577dfc0cc89d9c2e88cbbde27967
Roshan Bhandari <brishi98@gmail.com>
2014-07-18 16:54:33 +0545

    footable initialized

--- mainapp/static/js/ajax-handle.js
+++ mainapp/static/js/ajax-handle.js
@@ -266,12 +266,12 @@ function stockists_ajax(data){
 			new_html =  data['html'] + current_html;
 			$("#supplier_connections").html(new_html);
 			
-		 	/*$('#suppliersTable').trigger('footable_initialize');*/
-		 	$('#suppliersTable').footable();
-		 	$('#stockistsTable').footable();
-            /*$('#stockistsTable').trigger('footable_resize');*/
-            /*$('#suppliersTable').trigger('footable_initialize');*/
-            $('#suppliersTable').trigger('footable_resize');
+		  $('#suppliersTable').trigger('footable_initialize');
+          /*$('#stockistsTable').trigger('footable_resize');*/
+          $('#suppliersTable').trigger('footable_initialize');
+          /*$('#suppliersTable').trigger('footable_resize');*/
+/*          $('#suppliersTable').footable();
+		  $('#stockistsTable').footable();*/
 		}
 
         	$('#c_con_no').html(data['b_conn_no']);
@@ -304,12 +304,12 @@ function suppliers_ajax(data){
 
 			new_html =  data['html'] + current_html;
 			$('#stockist_connections').html(new_html);	
-			/*$('#stockistsTable').trigger('footable_initialize');
-          	$('#stockistsTable').trigger('footable_resize');
-          	$('#stockistsTable').trigger('footable_initialize');
-          	$('#stockistsTable').trigger('footable_resize');*/
-          	$('#suppliersTable').footable();
-		 	$('#stockistsTable').footable();
+		  $('#suppliersTable').trigger('footable_initialize');
+          /*$('#stockistsTable').trigger('footable_resize');*/
+          $('#suppliersTable').trigger('footable_initialize');
+          /*$('#suppliersTable').trigger('footable_resize');*/
+/*          $('#suppliersTable').footable();
+		  $('#stockistsTable').footable();*/
 		}
 
 
@@ -352,12 +352,12 @@ function get_next_page_conn_success(data){
 		}
 	}
 	else{
-		  /*$('#suppliersTable').trigger('footable_initialize');
-          $('#stockistsTable').trigger('footable_resize');
+		  $('#suppliersTable').trigger('footable_initialize');
+          /*$('#stockistsTable').trigger('footable_resize');*/
           $('#suppliersTable').trigger('footable_initialize');
-          $('#suppliersTable').trigger('footable_resize');*/
-          $('#suppliersTable').footable();
-		  $('#stockistsTable').footable();
+          /*$('#suppliersTable').trigger('footable_resize');*/
+/*          $('#suppliersTable').footable();
+		  $('#stockistsTable').footable();*/
 	}
 }
 
