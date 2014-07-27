51d9e821a645d146e40f0a061815c079459bbfcf
Roshan Bhandari <brishi98@gmail.com>
2014-07-27 16:57:56 +0545

    server email config changes

--- foodtrade/settings_server.py
+++ foodtrade/settings_server.py
@@ -24,7 +24,7 @@ DATABASES = {
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
-DEFAULT_FROM_EMAIL = 'FoodTrade <no-reply@foodtrade.com>'
+# DEFAULT_FROM_EMAIL = 'FoodTrade <no-reply@foodtrade.com>'
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
