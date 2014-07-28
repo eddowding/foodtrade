607ad688e132cabb0745b9139a884bd49d8794bb
Roshan Bhandari <brishi98@gmail.com>
2014-07-28 14:22:11 +0545

    changes

--- foodtrade/settings_remote.py
+++ foodtrade/settings_remote.py
@@ -155,6 +155,7 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     "djstripe",
     "floppyforms",
+    "bootstrapform",
 )
 
 # A sample logging configuration. The only tangible logging
--- foodtrade/settings_server.py
+++ foodtrade/settings_server.py
@@ -152,6 +152,7 @@ INSTALLED_APPS = (
     'django.contrib.admindocs',
     "djstripe",
     "floppyforms",
+    "bootstrapform",
 )
 
 # A sample logging configuration. The only tangible logging
