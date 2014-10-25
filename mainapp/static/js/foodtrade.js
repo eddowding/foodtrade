b0556918c837e25e71d1f213a35fc2695d6e9272
sujitmah <sujitmhj@gmail.com>
2014-10-25 15:16:43 +0545

    next push

--- mainapp/static/js/foodtrade.js
+++ mainapp/static/js/foodtrade.js
@@ -6,16 +6,13 @@ var placeCord;
 function initialize() {
 
   // Create the search box and link it to the UI element.
-  var input = /** @type {HTMLInputElement} */(
-      document.getElementById('pac_input_market'));
+  // var input = /** @type {HTMLInputElement} */(      document.getElementById('pac_input_market'));
   var input_profile = (document.getElementById('pac_input_profile'));
 
-  var searchBox = new google.maps.places.SearchBox(
-    /** @type {HTMLInputElement} */(input));
+  // var searchBox = new google.maps.places.SearchBox(    /** @type {HTMLInputElement} */(input));
 
-  var profile_address = new google.maps.places.SearchBox((input_profile));
-
-
+  // var profile_address = new google.maps.places.SearchBox((input_profile));
+  var profile_address = new google.maps.places.Autocomplete(input_profile);
 
   google.maps.event.addListener(input_profile, 'places_changed', function() {
     loading_latlng = true;
@@ -34,14 +31,14 @@ function initialize() {
   // [START region_getplaces]
   // Listen for the event fired when the user selects an item from the
   // pick list. Retrieve the matching places for that item.
-  google.maps.event.addListener(searchBox, 'places_changed', function() {
-    loading_latlng = true;
-  var places = searchBox.getPlaces();
-    placeCord = places[0].geometry.location;
-    $("#lon").val(placeCord.lng());
-    $("#lat").val(placeCord.lat());
-    get_address("market");
-  });
+  // google.maps.event.addListener(searchBox, 'places_changed', function() {
+  //   loading_latlng = true;
+  // var places = searchBox.getPlaces();
+  //   placeCord = places[0].geometry.location;
+  //   $("#lon").val(placeCord.lng());
+  //   $("#lat").val(placeCord.lat());
+  //   get_address("market");
+  // });
 
 
 }
