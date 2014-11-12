d7935f71db80865cd97810559db31c7fe9e89a39
Jaideep Das <jaideep.jdof@gmail.com>
2014-11-12 08:57:28 +0530

    added basic views for search

--- search/urls.py
+++ search/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('search.views',
     url(r'^$', 'search', name='search'),
+    url(r'result/$', 'search_result', name='search_result'),
 )
--- search/views.py
+++ search/views.py
@@ -1,6 +1,18 @@
+import json
 from django.shortcuts import render
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from django.template import RequestContext
+from mainapp.classes.TweetFeed import UserProfile
 
 
 def search(request):
+    if request.user.is_authenticated():
+        user_profile_obj = UserProfile()
+        user_profile = user_profile_obj.get_profile_by_id(str(request.user.id))
+        print user_profile
     return render(request, 'search.html')
+
+def search_result(request):
+    html = render_to_string('_partials/search_result.html', {}, context_instance=RequestContext(request))
+    return HttpResponse(json.dumps({'html': html}))
