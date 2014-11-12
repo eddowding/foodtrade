e5ce770eb093325c2a1d3c889f083ffcbdd69ad6
Jaideep Das <jaideep.jdof@gmail.com>
2014-11-12 08:57:28 +0530

    added basic views for search

---
+++ search/static/js/searchapp.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  var map = L.map('map').setView([51.505, -0.09], 13);
+  L.tileLayer('https://{s}.tiles.mapbox.com/v3/foodtrade.i26pc0n5/{z}/{x}/{y}.png', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18
+  }).addTo(map);
+
+  $('#search_query').keyup(function (ev) {
+    var query = $(this).val();
+    if ((query.length % 3)===0) {
+      $.ajax({
+        url: '/search/result',
+        method: 'get',
+        dataType: 'json',
+        success: function (data) {
+          $('#search_results').html(data.html);
+        }
+      });
+    }
+  });
+});
