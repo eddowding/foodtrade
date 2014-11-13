cb5354febe0b6aa023a951b967707666feaa933d
Jaideep Das <jaideep.jdof@gmail.com>
2014-11-13 09:36:47 +0530

    added geo query logic

--- search/views.py
+++ search/views.py
@@ -9,14 +9,23 @@ from mainapp.classes.TweetFeed import UserProfile
 
 
 def search(request):
-    if request.user.is_authenticated():
-        user_profile_obj = UserProfile()
-        user_profile = user_profile_obj.get_profile_by_id(str(request.user.id))
-        print user_profile
     return render(request, 'search.html')
 
 def search_result(request): #TODO: change result div name, change hard coded url.
     query = request.GET.get('query')
+    lat = request.GET.get('lat')
+    lng = request.GET.get('lng')
+    distance = request.GET.get('distance')
+    if request.user.is_authenticated():
+        user_profile_obj = UserProfile()
+        user_profile = user_profile_obj.get_profile_by_id(str(request.user.id))
+        if user_profile.get('latlng') and user_profile.get('latlng').get('coordinates'):
+            lat = user_profile.get('latlng').get('coordinates')[1]
+            lng = user_profile.get('latlng').get('coordinates')[0]
+            distance = '20km'
     sqs = S().indexes(settings.ES_INDEX).query(_all__fuzzy=query)
+    if lat and lng:
+        sqs = sqs.filter(latlng__distance=(distance, lat, lng))
     html = render_to_string('_partials/search_result.html', {'sqs': sqs}, context_instance=RequestContext(request))
-    return HttpResponse(json.dumps({'html': html}))
+    objs = list(sqs.values_dict('id', 'name', 'profile_image', 'description', 'latlng'))
+    return HttpResponse(json.dumps({'html': html, 'objs': objs}))
