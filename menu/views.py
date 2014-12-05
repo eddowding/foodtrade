61c96eb50e9ae1ba0b99b8a925a8a3f32e09923f
rajpaul <paul.rajib03@gmail.com>
2014-12-06 02:51:14 +0600

    dev: Rajib , Commi for templating menu

--- menu/views.py
+++ menu/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+
+# django import
 from django.http import HttpResponse
+from django.shortcuts import render , render_to_response
+from django.template import RequestContext
+
+#app import
+
+
 # Create your views here.
 
 def menu(request):
-    return HttpResponse('menu')
+    template_name = 'menu/menus.html'
+    var = {'title' : 'menu'}
+    contextt = RequestContext(request , var);
+    return render_to_response(template_name , contextt)
