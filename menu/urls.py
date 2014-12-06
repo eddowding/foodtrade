6559a27764bb7236547e7f4060f40301c2f30734
rajpaul <paul.rajib03@gmail.com>
2014-12-07 01:17:59 +0600

    dev: Rajib , Commit for registration and login page for menu

--- menu/urls.py
+++ menu/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('menu.views',
-    url(r'^$', 'menu', name='menu')
+    url(r'^$', 'menu', name='menu'),
+    url(r'^register/$', 'register', name='menu-registration'),
+    url(r'^login/$', 'login', name='menu-login')
 )
--- menu/views.py
+++ menu/views.py
@@ -5,12 +5,24 @@ from django.http import HttpResponseRedirect
 # Create your views here.
 
 def menu(request):
+    ''' Get list of menus '''    
     return render(request, 'menu/menus.html', {'title' : 'menu'})
 
 
 def register(request):
+    ''' User Registration in menu '''
+    
     if request.method == 'POST':
         #TODO: add user create logic
         return HttpResponseRedirect(reverse('menu'))
     else:
-        return render(request, 'register.html')
+        return render(request, 'menu/register.html')
+
+
+def login(request):
+    ''' User login in menu '''
+    if request.method == 'POST':
+        #TODO: add user login logic
+        return HttpResponseRedirect(reverse('menu'))
+    else:
+        return render(request, 'menu/login.html')
