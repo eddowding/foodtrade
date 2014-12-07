ad5d294b312e31669500052868166a62573d0a4c
Jaideep Das <jaideep.jdof@gmail.com>
2014-12-08 01:51:23 +0530

    added user to mongo admin

--- menu/mongoadmin.py
+++ menu/mongoadmin.py
@@ -1,7 +1,8 @@
+from mongoengine.django.auth import User
 from mongonaut.sites import MongoAdmin
 from menu.models import *
 
-
+User.mongoadmin = MongoAdmin()
 Region.mongoadmin = MongoAdmin()
 Authority.mongoadmin = MongoAdmin()
 BusinessType.mongoadmin = MongoAdmin()
