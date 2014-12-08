c497300debb726e6a50e5fe7bdec198b75333082
Jaideep Das <jaideep.jdof@gmail.com>
2014-12-09 01:50:23 +0530

    added validation for regiistration form

--- menu/static/js/register.js
+++ menu/static/js/register.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
+    $('input[name="first_name"]').focus();
+
+    $('.form-control').focusout(function(ev) {
+        var parentDiv = $(this).parent();
+        var helpTextDiv = parentDiv.find('.help-text');
+        if (!$(this).val()) {
+            parentDiv.addClass('has-error');
+            helpTextDiv.html('This is required.');
+            $('.signup').addClass('disabled');
+        } else {
+            parentDiv.removeClass('has-error');
+            helpTextDiv.html('');
+            $('.signup').addRemove('disabled');
+        }
+    });
+
     $('input[name="username"]').focusout(function(ev) {
         var username = $(this).val();
+        var parentDiv = $(this).parent();
+        var helpTextDiv = parentDiv.find('.help-text');
         $.ajax({
             url: '/menu/user/lookup/count',
             data: {
@@ -10,7 +28,13 @@ $(document).ready(function() {
             dataType: 'JSON',
             success: function(data) {
                 if (data.count > 0) {
-                    $('.register-msg').html('Username exists.').removeClass('hidden');
+                    parentDiv.addClass('has-error');
+                    helpTextDiv.html('Username already exists.');
+                    $('.signup').addClass('disabled');
+                } else {
+                    parentDiv.removeClass('has-error');
+                    helpTextDiv.html('');
+                    $('.signup').addRemove('disabled');
                 }
             }
         });
