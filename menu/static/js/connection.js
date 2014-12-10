66070de99704ff847e16c4d5720a67cd63b9b2ac
Jaideep Das <jaideep.jdof@gmail.com>
2014-12-11 00:39:49 +0530

    fixed suggestion

--- menu/static/js/connection.js
+++ menu/static/js/connection.js
@@ -1,18 +1,35 @@
 $(document).ready(function() {
     var establishments = new Bloodhound({
-        name: 'establishments',
-        remote: '/menu/establishment/lookup/name/?q=%QUERY',
-        datumTokenizer: function(d) {
-            return Bloodhound.tokenizers.whitespace(d);
+        remote: {
+            url: '/menu/establishment/lookup/name/?q=%QUERY',
+            filter: function(establishments) {
+                return establishments.objs;
+            }
+
+        },
+        datumTokenizer: function(establishment) {
+            return Bloodhound.tokenizers.whitespace(establishment.name);
         },
         queryTokenizer: Bloodhound.tokenizers.whitespace
     });
 
     establishments.initialize();
 
-    $('.typeahead').typeahead(null, {
-        name: 'establishments',
+    $('.connection').typeahead(null, {
         displayKey: 'name',
+        name: 'establishment',
         source: establishments.ttAdapter()
+    });
+
+    var connectionId = null;
+
+    $('.connection').on('typeahead:selected', function(ev, establishment) {
+        connectionId = establishment.value;
+        $('.add-connection').removeClass('disabled');
+    });
+
+    $('.add-connection').click(function(ev) {
+        var connectionType = $('input[name="connection-type"]').val();
+        console.log(connectionId, connectionType);
     });
 });
