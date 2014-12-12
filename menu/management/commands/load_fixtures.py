1128091b657eab1e2e01d08f2888a9b595d9e561
rajpaul <paul.rajib03@gmail.com>
2014-12-13 02:57:29 +0600

    dev:Rajib Commit for adding fixtures

---
+++ menu/management/commands/load_fixtures.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand, CommandError
+from menu.models import *
+import csv ,os
+import datetime
+
+
+class Command(BaseCommand):
+   # args = 'Entity type'
+
+    def handle(self, *args, **options):
+
+        type_match = False
+        if (os.path.exists(args[0])):
+            with open(args[0]) as csvfile:
+                reader = csv.reader(csvfile, delimiter=',')
+                for data in reader:
+                    for item in data:
+                        if (args[1] == 'allergen'):
+                            allergen = Allergen()
+                            allergen.name = item
+                            allergen.added_on = datetime.datetime.now()
+                            allergen.save()
+                            type_match = True
+                        elif (args[1] == 'meat'):
+                            meat = Meat()
+                            meat.name = item
+                            meat.added_on = datetime.datetime.now()
+                            meat.save()
+                            type_match = True
+                        elif (args[1] == 'gluten'):
+                            gluten = Gluten()
+                            gluten.name = item
+                            gluten.added_on = datetime.datetime.now()
+                            gluten.save()
+                            type_match = True
+
+        else:
+            print 'File doesn\'t exist'
+
+        if (not type_match):
+            print 'Type doesn\'t match '
+        else:
+            print 'Successfully added'
+
+
+
