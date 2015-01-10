9927bde6593120f620cc1dd0b6d6a5c149832d4b
Manuj Rastogi <manujrastogi@ManujMacBook.local>
2015-01-10 17:45:07 +0530

    fixed fabfile collectstatic command

--- fabfile.py
+++ fabfile.py
@@ -10,7 +10,7 @@ def deploy():
     sudo('cd /var/www; chown -R foodtrade:foodtrade bower_components')
     run('cd /var/www; ./manage.py bower install')
     sudo('cd /var/www; chown -R root:root bower_components')
-    sudo('cd /var/www; ./manage.py collectstatic -l --noinput')
+    sudo('cd /var/www; ./manage.py collectstatic --noinput')
     sudo('chown www-data:www-data -R /var/www')
     sudo('service uwsgi restart')
     sudo('service nginx restart')
