71a91e09418373e0654e478c99243136d10036b8
Jaideep Das <jaideep.jdof@gmail.com>
2015-01-20 01:54:03 +0530

    added child logic

--- test_iteration.py
+++ test_iteration.py
@@ -3,6 +3,7 @@ from dominate.tags import *
 
 ingredient_tree = '[[{"ingredientId":"54b6d44e0eaefa26f5c88ffd","dishId":"54b4e15d0eaefa5f038d177c","children":[[{"ingredientId":"54b8c8930eaefa04833c5d59","dishId":"54b4e15d0eaefa5f038d177c","children":[[]]},{"ingredientId":"54b8c89f0eaefa04833c5d5a","dishId":"54b4e15d0eaefa5f038d177c","children":[[]]},{"ingredientId":"54b8c8a60eaefa04833c5d5b","dishId":"54b4e15d0eaefa5f038d177c","children":[[]]},{"ingredientId":"54b8c8ae0eaefa04833c5d5c","dishId":"54b4e15d0eaefa5f038d177c","children":[[{"ingredientId":"54ba4a720eaefa44fd67a5df","dishId":"54b4e15d0eaefa5f038d177c","children":[[]]}]]},{"ingredientId":"54b8c8c60eaefa04833c5d5d","dishId":"54b4e15d0eaefa5f038d177c","children":[[]]}]]},{"ingredientId":"54ba4a790eaefa44fd67a5e0","dishId":"54b4e15d0eaefa5f038d177c","children":[[]]}]]'
 html = ul(cls='ingredient-tree', data_dish_id='54b4e15d0eaefa5f038d177c')
+parent_li_ul_ref = {}
 
 def walk(ingredients, parent=None, parent_li_ul=None):
     for ingredient in ingredients:
@@ -10,15 +11,19 @@ def walk(ingredients, parent=None, parent_li_ul=None):
             walk(ingredient, parent, parent_li_ul)
         elif isinstance(ingredient, dict) and len(ingredient['children'][0]):
             # print {'dishId': ingredient['dishId'], 'parentId': parent, 'ingredientId': ingredient['ingredientId']}
-            if parent_li_ul:
-                parent_li = parent_li_ul.add(li(str(parent), data_dish_id=ingredient['dishId'], data_ingredient_id=ingredient['ingredientId'], cls="ingredient-item"))
+            if parent:
+                parent_li = parent_li_ul_ref[parent].add(li(str(parent), data_dish_id=ingredient['dishId'], data_ingredient_id=ingredient['ingredientId'], cls="ingredient-item"))
             else:
                 parent_li = html.add(li(str(parent), data_dish_id=ingredient['dishId'], data_ingredient_id=ingredient['ingredientId'], cls="ingredient-item"))
+
             parent_li_ul = parent_li.add(ul())
+
+            if not ingredient['ingredientId'] in parent_li_ul_ref:
+                parent_li_ul_ref[ingredient['ingredientId']] = parent_li_ul
             walk(ingredient['children'], ingredient['ingredientId'], parent_li_ul)
         else:
-            if parent_li_ul:
-                parent_li = parent_li_ul.add(li(str(parent), data_dish_id=ingredient['dishId'], data_ingredient_id=ingredient['ingredientId'], cls="ingredient-item"))
+            if parent:
+                parent_li = parent_li_ul_ref[parent].add(li(str(parent), data_dish_id=ingredient['dishId'], data_ingredient_id=ingredient['ingredientId'], cls="ingredient-item"))
                 parent_li.add(ul())
             # print {'dishId': ingredient['dishId'], 'parentId': parent, 'ingredientId': ingredient['ingredientId']}
 
