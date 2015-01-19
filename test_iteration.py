6e1066913f416f333968810c6214c149230f09ee
Jaideep Das <jaideep.jdof@gmail.com>
2015-01-20 01:13:21 +0530

    added basic logic to render tempalte

--- test_iteration.py
+++ test_iteration.py
@@ -1,18 +1,32 @@
 import json
+from dominate.tags import *
 
 ingredient_tree = '[[{"ingredientId":"54b6d44e0eaefa26f5c88ffd","dishId":"54b4e15d0eaefa5f038d177c","children":[[{"ingredientId":"54b8c8930eaefa04833c5d59","dishId":"54b4e15d0eaefa5f038d177c","children":[[]]},{"ingredientId":"54b8c89f0eaefa04833c5d5a","dishId":"54b4e15d0eaefa5f038d177c","children":[[]]},{"ingredientId":"54b8c8a60eaefa04833c5d5b","dishId":"54b4e15d0eaefa5f038d177c","children":[[]]},{"ingredientId":"54b8c8ae0eaefa04833c5d5c","dishId":"54b4e15d0eaefa5f038d177c","children":[[{"ingredientId":"54ba4a720eaefa44fd67a5df","dishId":"54b4e15d0eaefa5f038d177c","children":[[]]}]]},{"ingredientId":"54b8c8c60eaefa04833c5d5d","dishId":"54b4e15d0eaefa5f038d177c","children":[[]]}]]},{"ingredientId":"54ba4a790eaefa44fd67a5e0","dishId":"54b4e15d0eaefa5f038d177c","children":[[]]}]]'
+html = ul(cls='ingredient-tree', data_dish_id='54b4e15d0eaefa5f038d177c')
 
-def walk(ingredients, parent=None):
+def walk(ingredients, parent=None, parent_li_ul=None):
     for ingredient in ingredients:
         if isinstance(ingredient, list):
-            walk(ingredient, parent)
+            walk(ingredient, parent, parent_li_ul)
         elif isinstance(ingredient, dict) and len(ingredient['children'][0]):
-            print {'dishId': ingredient['dishId'], 'parentId': parent, 'ingredientId': ingredient['ingredientId']}
-            walk(ingredient['children'], ingredient['ingredientId'])
+            # print {'dishId': ingredient['dishId'], 'parentId': parent, 'ingredientId': ingredient['ingredientId']}
+            if parent_li_ul:
+                print 'Branch & Leaf'
+                parent_li = parent_li_ul.add(li(str(parent), data_dish_id=ingredient['dishId'], data_ingredient_id=ingredient['ingredientId'], cls="ingredient-item"))
+            else:
+                print 'Branch'
+                parent_li = html.add(li(str(parent), data_dish_id=ingredient['dishId'], data_ingredient_id=ingredient['ingredientId'], cls="ingredient-item"))
+            parent_li_ul = parent_li.add(ul())
+            walk(ingredient['children'], ingredient['ingredientId'], parent_li_ul)
         else:
-            print {'dishId': ingredient['dishId'], 'parentId': parent, 'ingredientId': ingredient['ingredientId']}
+            if parent_li_ul:
+                print 'Leaf'
+                parent_li = parent_li_ul.add(li(str(parent), data_dish_id=ingredient['dishId'], data_ingredient_id=ingredient['ingredientId'], cls="ingredient-item"))
+                parent_li_ul = parent_li.add(ul())
+            # print {'dishId': ingredient['dishId'], 'parentId': parent, 'ingredientId': ingredient['ingredientId']}
 
 
 if __name__ == '__main__':
     ingredients = json.loads(ingredient_tree)
     walk(ingredients)
+    print html
