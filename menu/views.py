c14b8183f091225c0ff6f9b7e1cbee2cf24808f8
Jaideep Das <jaideep.jdof@gmail.com>
2015-03-01 22:36:25 +0530

    restrict auto complete ingredient to top level

--- menu/views.py
+++ menu/views.py
@@ -409,7 +409,7 @@ def ingredient_lookup_name(request):
     for klass in klass_list:
         for obj in klass.objects.filter(**query2):
             tmp_list.append({'class': str(klass.__name__), 'id': str(obj.id), 'name': obj.name})
-    for i in Ingredient.objects.filter(name__icontains=keyword)[0:10]:
+    for i in Ingredient.objects.filter(name__icontains=keyword, parent=None)[0:10]:
         tmp_list.append({'class': str(Ingredient.__name__), 'id': str(i.id), 'name': i.name})
     return HttpResponse(json.dumps({'status': True, 'objs': tmp_list[0:10]}))
 
