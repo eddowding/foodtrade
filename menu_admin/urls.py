0fd27a12c1f59e871a76b0d4e1856a4304cf3457
Jaideep Das <jaideep.jdof@gmail.com>
2015-03-12 15:34:13 +0530

    added user detail view

--- menu_admin/urls.py
+++ menu_admin/urls.py
@@ -3,5 +3,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = patterns('menu_admin.views',
-    url(r'^user/', 'admin_user', name='menu_admin_user')
+    url(r'^user/$', 'admin_user', name='menu_admin_user'),
+    url(r'^user/(?P<id>[\w]+)/$', 'admin_user_detail', name='menu_admin_user_detail'),
 )
--- menu_admin/views.py
+++ menu_admin/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import Http404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib.auth.decorators import login_required
+from django.core.urlresolvers import reverse_lazy
+from bson.objectid import ObjectId
 from mongoengine.django.auth import User
 
+
+@login_required(login_url=reverse_lazy('menu-login'))
 def admin_user(request):
+    if not request.user.is_superuser:
+        raise Http404
     users = User.objects.all()
     paginator = Paginator(users, 5)
 
@@ -16,3 +23,11 @@ def admin_user(request):
         users = paginator.page(paginator.num_pages)
 
     return render(request, 'user.html', {'users': users})
+
+
+@login_required(login_url=reverse_lazy('menu-login'))
+def admin_user_detail(request, id):
+    if not request.user.is_superuser:
+        raise Http404
+    user = User.objects.get(pk=ObjectId(id))
+    return render(request, 'user_detail.html', {'user': user})
